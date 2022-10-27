88cfb97152bcf58891c70d0e8023f973a3a3b52d
Brenan Marenger <104685201+BrenanMarenger@users.noreply.github.com>
2022-10-27 18:20:38 -0400

    Update dirCount.cpp

--- dirCount.cpp
+++ dirCount.cpp
@@ -9,49 +9,52 @@
 #include<assert.h>
 #include <stdio.h>
 #include <vector>
-
+#include<cstring>
 using namespace std;
 
 int size = 0;
 vector<int> inode;
-bool flag = false;
+
+
+bool checkInode(int node) { //return true if already saw file
+	for (auto i : inode) {
+		if (inode[i] == node) {
+			return true;
+		}
+	}
+	inode.push_back(node);
+	return false;
+}
 
 void recDir(const char *path) { 
+
 	DIR* dir;
 	struct dirent* ent;
 	struct stat data;
 
-	assert((opendir(path)) != NULL);
-	if ((opendir(path)) != NULL) { //opens
-		auto dir = opendir(path);
+	if ((dir = opendir(path)) != NULL) { //opens
+		assert(dir != nullptr);
 		while ((ent = readdir(dir)) != NULL) { //reads each entry
-
 			string fullPath = path;
 			fullPath += '/';
 			fullPath += ent->d_name;
 			cout << fullPath << " ";
-
 			if (ent->d_type == DT_DIR) { //directory
 				cout << "[D]" << endl;
-				if (ent->d_name[0] != '.') {
+				if ((strcmp(ent->d_name, ".")) && (strcmp(ent->d_name, ".."))) {
 					recDir(fullPath.c_str());
 				}
 			}
 			else if (ent->d_type == DT_REG) { //file
 				if (stat(fullPath.c_str(), &data) != -1) {
 					cout << "[F, " << data.st_size << "]";
-
-					for (auto i : inode) {
-						if (inode[i] == data.st_ino) {
-							flag = true; 
-						}
-					}
-					inode.push_back(data.st_ino); //prevent links repeating total size
-					if (flag != true) {
+		
+					/*
+					if (checkInode(data.st_ino) == false) { //check for soft link
 						size += data.st_size;
 					}
-					flag = false;
-
+					*/
+					
 					printf(" Permissions: ");
 					printf((S_ISDIR(data.st_mode)) ? "d" : "-");
 					printf((data.st_mode & S_IRUSR) ? "r" : "-");
@@ -65,30 +68,13 @@ void recDir(const char *path) {
 					printf((data.st_mode & S_IXOTH) ? "x" : "-");
 					printf("\n");
 				}
-				else if (stat(fullPath.c_str(), &data) == -1) {
-					perror("read file was bad: ");
-					exit(1);
-				}
 			}
 			else if (ent->d_type == DT_LNK) {//link
-				char buf[150];
-				auto ret = readlink(fullPath.c_str(), buf, 150);
-				if (ret == -1) {
-					perror("read link was bad: ");
-					exit(1);
-				}
-				else {
-					cout << "[L" << "->";
-					for (int i = 0; i < 150; i++) { //finding link to
-						if (buf[i] > 0) {
-							cout << buf[i];
-						}
-						else {
-							break;
-						}
-					}
-					cout << "]"<< endl;
-				}
+				char buf[100];
+				auto ret = readlink(fullPath.c_str(), buf, 100);
+				assert(ret != -1);		
+				buf[ret] = 0;
+				cout << "[L" << "->" << buf << "]"<< endl;	
 			}
 		}
 		closedir(dir);
